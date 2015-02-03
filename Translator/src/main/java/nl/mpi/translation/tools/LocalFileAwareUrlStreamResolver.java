7b332733c38ef5658f6a6e60025f08dc02ede846
Twan Goosen <twan.goosen@mpi.nl>
2015-02-03 09:05:50 +0100

    Created local file aware url stream resolver class

---
+++ Translator/src/main/java/nl/mpi/translation/tools/LocalFileAwareUrlStreamResolver.java
@@ -0,0 +1,82 @@
+/*
+ * Copyright (C) 2015 Max Planck Institute for Psycholinguistics
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ */
+package nl.mpi.translation.tools;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+
+/**
+ * A URL stream resolver that uses a provided mapping between a base URL and a
+ * base file path to read files from the local filesystem even if they are
+ * requested through their public URL
+ *
+ * @author Twan Goosen <twan.goosen@mpi.nl>
+ */
+public class LocalFileAwareUrlStreamResolver implements UrlStreamResolver {
+
+    private final UrlStreamResolver baseResolver;
+
+    private final String baseUrl;
+    private final File basePath;
+
+    /**
+     * Constructs the file aware resolver with a new instance of
+     * {@link UrlStreamResolverImpl} as base resolver
+     *
+     * @param baseResolver resolver to use if no corresponding file can be
+     * found
+     * @param baseUrl base URL
+     * @param basePath file object representing the base path that corresponds
+     * with the base URL
+     */
+    public LocalFileAwareUrlStreamResolver(UrlStreamResolver baseResolver, String baseUrl, File basePath) {
+        this.baseResolver = baseResolver;
+        this.baseUrl = baseUrl;
+        this.basePath = basePath;
+    }
+
+    /**
+     * Constructs the file aware resolver with a new instance of
+     * {@link UrlStreamResolverImpl} as base resolver
+     *
+     * @param baseUrl base URL
+     * @param basePath file object representing the base path that corresponds
+     * with the base URL
+     */
+    public LocalFileAwareUrlStreamResolver(String baseUrl, File basePath) {
+        this(new UrlStreamResolverImpl(), baseUrl, basePath);
+    }
+
+    @Override
+    public InputStream getStream(URL url) throws IOException {
+        final String urlString = url.toString();
+        if (urlString.startsWith(baseUrl)) {
+            final String child = urlString.substring(baseUrl.length());
+            final File localFile = new File(basePath, child);
+            if (localFile.exists()) {
+                return new FileInputStream(localFile);
+            } // else fall back to base resolver
+        }
+        // no match on base URL or file does not exist
+        return baseResolver.getStream(url);
+    }
+
+}
